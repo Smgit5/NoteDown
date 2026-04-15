a9608a604ba9596ac0b7436ea9ef7c57a4b47c36
Suman <sumanmaji349@gmail.com>
2026-04-15 23:09:19 +0530

    first commit

---
+++ src/main/java/com/suman/notedown/NotedownApplication.java
@@ -0,0 +1,13 @@
+package com.suman.notedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotedownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotedownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suman/notedown/NotedownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suman.notedown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotedownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
